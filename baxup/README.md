48cd4d320b49cb6dd0ec442bc4126951243201bf
adrian-griffin <adrianrilay@protonmail.com>
2024-09-20 23:03:04 -0700

    Add several error checks, option Docker flag

---
+++ baxup/README.md
@@ -0,0 +1,30 @@
+# baxup
+
+Performs backup on target directory and its contents, storing them as a `*.bak.tar.gz` compression file to save on space. Optionally, the newly backed-up `.tar.gz` file can be copied to a remote machine when passed flags to do so. 
+
+Baxup can be used alongside crontabs to perform both local and remote backups on a schedule. 
+
+
+
+
+
+Procli is a Debian/Ubuntu machine CLI provisioning tool that installs some of the most basic packages, QoL changes, and tools that I prefer to have on most of my machines. 
+
+Procli is by default configured to download the following linux packages, OhMyZsh custom plugins, and extra repositories for tools. This can be adjusted with some mediocre knowledge of Go. 
+
+Linux packages installed:
+- `Zsh`
+- `Git`
+- `Nvim`
+- `fzf` (for fzf searching via OhMyZsh)
+
+OhMyZsh Custom Plugins & Customizations:
+- `zsh-autosuggestions`
+- `zsh-syntax-highlighting`
+- `fzf-zsh-plugin`
+
+Extra Tools Downloaded
+
+- `/adrian-griffin/go-tools/`
+
+Procli also, by default, performs a `git clone` or `git pull` on a remote GitHub repository and stores the repo at `$HOME/dotties` wherein several custom dotfiles can be cloned to the machine for easy dotfile standardization. By default Procli only moves a `.zshrc` file from `$HOME/dotties` > `$HOME/.zshrc`.
--- baxup/baxup.go
+++ baxup/baxup.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	//"os"
+	"os"
 	"os/exec"
-	//"os/user"
 	"path/filepath"
 	"flag"
 	"log"
@@ -13,70 +12,86 @@ import (
 // runCommand function, requires command name (docker); accepts multiple arguments
 func runCommand(commandName string, args ...string) error {
 	cmd := exec.Command(commandName, args...)
-	cmd.Stdout = nil
-	cmd.Stderr = nil
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	return cmd.Run()
 }
-
 
 func main () {
 	//// IMPORTANT: In order to change the default docker backup & docker compose container root paths, these variables much be changed manually and the script rebuilt!
 	//// Don't forget the trailing `/`
 
 	// Defines the root directories for all docker compose container directories, as well as the directory to store compressed backups 
-	dockerComposeRootPath := "/opt/docker/"
-	dockerBackupRootPath := "/opt/docker-backups/"
+	targetRootPath := "/opt/docker/"
+	backupRootPath := "/opt/docker-backups/"
 
 	// Flag definitions
-	dockerContainerName := flag.String("docker-target", "vaultwarden", "Name of docker container's directory to back up.")
-	remoteSend := flag.Bool("remote-send", false, "Enable sending backup .tar.gz file to remote server. Additional flags needed.")
-	remoteUser := flag.String("remote-user", "agriffin", "Remote machine username. SSH key required.")
-	remoteHost := flag.String("remote-host", "10.115.0.1", "Remote machine IP address.  SSH key required.")
-	remoteFile := flag.String("remote-file", "", "Remote filepath. Defaults to /home/$USER/$DOCKERNAME.bak.tar.gz")
+	targetName := flag.String("target-name", "", "Name of target directory, not including rooth path")
+	remoteSend := flag.Bool("remote-send", false, "Enable sending backup file to remote machine. Additional flags needed.")
+	remoteUser := flag.String("remote-user", "", "Remote machine username. SSH key required.")
+	remoteHost := flag.String("remote-host", "", "Remote machine IP address. SSH key required.")
+	remoteFile := flag.String("remote-file", "", "Remote filepath. Defaults to /home/$USER/$TARGETNAME.bak.tar.gz")
+	dockerBool := flag.Bool("docker", false, "Docker target? Default: false")
 
 	flag.Parse()
-	
-	sourceDir := filepath.Join(dockerComposeRootPath, *dockerContainerName, "/")
-	backupFile := filepath.Join(dockerBackupRootPath, *dockerContainerName+".bak.tar.gz")
 
-	// If remoteFile not specified, defaults to /home/$USER/$DOCKERNAME.bak.tar.gz
+	sourceDir := filepath.Join(targetRootPath, *targetName)
+	backupFile := filepath.Join(backupRootPath, *targetName+".bak.tar.gz")
+
+	// If remoteFile not specified, defaults to /home/$USER/$TARGETNAME.bak.tar.gz
 	if *remoteFile == "" {
-		*remoteFile = fmt.Sprintf("/home/%s/%s.bak.tar.gz", *remoteUser, *dockerContainerName)
+		if *remoteUser == "" {
+			log.Fatalf("Remote user must be specified when remote file path is not provided.")
+		}
+		*remoteFile = fmt.Sprintf("/home/%s/%s.bak.tar.gz", *remoteUser, *targetName)
 	}
 
-	// Step 1: Stop docker container
-	fmt.Println("Stopping Docker container . . .")
-	fmt.Println("Issuing docker compose down on ", filepath.Join(sourceDir, "docker-compose.yml"))
-	err := runCommand("docker", "compose", "-f", filepath.Join(sourceDir, "docker-compose.yml"), "down")
-	if err != nil {
-		log.Fatalf("Error stopping Docker container: %v", err)
+	if *targetName == "" {
+		fmt.Println("Target must be specified!")
+		fmt.Println("Exiting..")
+		os.Exit(1)
 	}
 
-	// Step 2: Compress target directory
+	if *dockerBool {
+		// Stop docker container
+		fmt.Println("Stopping Docker container . . .")
+		fmt.Println("Issuing docker compose down on ", filepath.Join(sourceDir, "docker-compose.yml"))
+		err := runCommand("docker", "compose", "-f", filepath.Join(sourceDir, "docker-compose.yml"), "down")
+		if err != nil {
+			log.Fatalf("Error stopping Docker container: %v", err)
+		}
+	}
+
+	// Compress target directory
 	fmt.Println("Compressing container directory . . .")
-	err = runCommand("tar", "-cvzf", backupFile, "-C", dockerComposeRootPath, *dockerContainerName)
-	fmt.Println("Backup file saved at:", backupFile)
+	err := runCommand("tar", "-cvzf", backupFile, "-C", targetRootPath, *targetName)
 	if err != nil {
 		log.Fatalf("Error compressing directory: %v", err)
 	}
+	fmt.Println("Backup file saved at:", backupFile)
 
 	// Optional: Rsync to remote destination
 	if *remoteSend {
+		if *remoteUser == "" || *remoteHost == "" {
+			log.Fatalf("Remote user and host must be specified when sending to a remote machine.")
+		}
 		fmt.Println("Copying to remote machine . . .")
 		// Checksum forced
 		rsyncArgs := []string{
 			"-avz", "--checksum", backupFile, fmt.Sprintf("%s@%s:%s", *remoteUser, *remoteHost, *remoteFile),
 		}
-		err = runCommand("rsync",rsyncArgs...)
+		err = runCommand("rsync", rsyncArgs...)
 		if err != nil {
 			log.Fatalf("Error sending file to remote server: %v", err)
 		}
 	}
 
-	// Restart docker container
-	fmt.Println("Starting Docker container . . .")
-	err = runCommand("docker", "compose", "-f", filepath.Join(sourceDir, "docker-compose.yml"), "up", "-d")
-	if err != nil {
-		log.Fatalf("Error starting Docker container: %v", err)
+	if *dockerBool {
+		// Restart docker container
+		fmt.Println("Starting Docker container . . .")
+		err = runCommand("docker", "compose", "-f", filepath.Join(sourceDir, "docker-compose.yml"), "up", "-d")
+		if err != nil {
+			log.Fatalf("Error starting Docker container: %v", err)
+		}
 	}
 }
